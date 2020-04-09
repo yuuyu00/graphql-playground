b7a8fdb55282dd3759584dc79cda788e1a6af009
yuuyu00 <my219813@gmail.com>
2020-04-10 01:34:59 +0900

    typescript-eslintを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
   env: {
-    es6: true,
     node: true,
   },
-  extends: "eslint:recommended",
-  plugins: ["typeorm", "graphql"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: ["@typescript-eslint", "typeorm", "graphql"],
   globals: {
     Atomics: "readonly",
     SharedArrayBuffer: "readonly",
   },
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: "module",
