3523ac18b1d6fdb9357cfec93876c086d4d3025c
yuuyu00 <my219813@gmail.com>
2020-08-22 00:28:53 +0900

    frontendパッケージでApolloClientを使えるように雛形を整えた

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,31 +1,31 @@
-import express from 'express'
-import 'reflect-metadata'
-import { ApolloServer, gql } from 'apollo-server-express'
-import { readFileSync } from 'fs'
-import { createConnection } from 'typeorm'
+import express from "express";
+import "reflect-metadata";
+import { ApolloServer, gql } from "apollo-server-express";
+import { readFileSync } from "fs";
+import { createConnection } from "typeorm";
 
-import { resolvers } from './resolvers'
-import { context } from './resolvers/context'
+import { resolvers } from "./resolvers";
+import { context } from "./resolvers/context";
 
-createConnection()
+createConnection();
 
-const schemaStr = readFileSync('src/schema/dist/schema.gql', 'utf8')
-console.log(schemaStr)
+const schemaStr = readFileSync("src/schema/dist/schema.gql", "utf8");
+console.log(schemaStr);
 const typeDefs = gql`
   ${schemaStr}
-`
+`;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context,
-  debug: process.env.NODE_ENV === 'development',
-})
+  debug: process.env.NODE_ENV === "development",
+});
 
-const app = express()
-app.disable('x-powerd-by')
-server.applyMiddleware({ app })
+const app = express();
+app.disable("x-powerd-by");
+server.applyMiddleware({ app });
 
-const PORT = 3000
+const PORT = 5000;
 app.listen(PORT, () => {
-  console.log(`GraphQL server ready at localhost:${PORT}/graphql`)
-})
+  console.log(`GraphQL server ready at localhost:${PORT}/graphql`);
+});
